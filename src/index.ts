ba3bcb31f4f753d0c9e104e4c92006f03879e165
Erin Millard <contact@erin.id.au>
2022-09-23 18:03:15 +1000

    Implement boolean variables

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,28 @@ interface Variable<T, O extends Options<T>> {
   value: () => O["required"] extends false ? T | undefined : T;
 }
 
+export function boolean<O extends Options<boolean>>(
+  name: string,
+  _description: string,
+  { default: d, required }: O
+): Variable<boolean, O> {
+  return {
+    value() {
+      const v = process.env[name];
+
+      if (typeof v === "string" && v != "") {
+        if (v === "true") return true;
+        if (v === "false") return false;
+        throw new InvalidBooleanError(name, v);
+      }
+
+      if (required && d == null) throw new UndefinedError(name);
+
+      return d;
+    },
+  } as Variable<boolean, O>;
+}
+
 export function string<O extends Options<string>>(
   name: string,
   _description: string,
@@ -31,5 +53,15 @@ export function initialize(): void {
 class UndefinedError extends Error {
   constructor(name: string) {
     super(`${name} is undefined and does not have a default value`);
+  }
+}
+
+class InvalidBooleanError extends Error {
+  constructor(name: string, value: string) {
+    const quotedValue = JSON.stringify(value);
+
+    super(
+      `The value of ${name} (${quotedValue}) is invalid: expected either "true" or "false".`
+    );
   }
 }
---
+++ test/suite/boolean.spec.ts
@@ -0,0 +1,269 @@
+import { boolean, initialize } from "../../src";
+import { hasType } from "../helpers";
+
+describe("Boolean variables", () => {
+  let env: typeof process.env;
+
+  beforeEach(() => {
+    env = process.env;
+    process.env = { ...env };
+  });
+
+  afterEach(() => {
+    process.env = env;
+  });
+
+  describe("when the variable is required", () => {
+    it("returns a boolean value", () => {
+      const variable = boolean("AUSTENITE_BOOLEAN_A", "description-a", {
+        required: true,
+      });
+
+      process.env.AUSTENITE_BOOLEAN_A = "false";
+      initialize();
+      const actual = variable.value();
+
+      expect(hasType<boolean, typeof actual>(actual)).toBeNull();
+    });
+
+    describe("when the value is one of the accepted literals", () => {
+      describe(".value()", () => {
+        it.each`
+          name                     | value      | expected
+          ${"AUSTENITE_BOOLEAN_A"} | ${"true"}  | ${true}
+          ${"AUSTENITE_BOOLEAN_B"} | ${"false"} | ${false}
+        `(
+          "returns the value associated with the literal ($value)",
+          ({
+            name,
+            value,
+            expected,
+          }: {
+            name: string;
+            value: string;
+            expected: boolean;
+          }) => {
+            const variable = boolean(name, "description-a", { required: true });
+
+            process.env[name] = value;
+            initialize();
+
+            expect(variable.value()).toBe(expected);
+          }
+        );
+      });
+    });
+
+    describe("when the value is invalid", () => {
+      describe(".value()", () => {
+        it.each`
+          name                     | value      | message
+          ${"AUSTENITE_BOOLEAN_A"} | ${"ture"}  | ${'The value of AUSTENITE_BOOLEAN_A ("ture") is invalid: expected either "true" or "false".'}
+          ${"AUSTENITE_BOOLEAN_B"} | ${"flase"} | ${'The value of AUSTENITE_BOOLEAN_B ("flase") is invalid: expected either "true" or "false".'}
+        `(
+          "throws ($name)",
+          ({
+            name,
+            value,
+            message,
+          }: {
+            name: string;
+            value: string;
+            message: string;
+          }) => {
+            const variable = boolean(name, "description-a", {
+              required: true,
+            });
+
+            process.env[name] = value;
+            initialize();
+
+            expect(() => {
+              variable.value();
+            }).toThrow(message);
+          }
+        );
+      });
+    });
+
+    describe.each`
+      label          | emptyValue
+      ${"undefined"} | ${undefined}
+      ${"empty"}     | ${""}
+    `(
+      "when the value is $label",
+      ({ emptyValue }: { emptyValue: string | undefined }) => {
+        describe("when there is a default value", () => {
+          describe(".value()", () => {
+            it.each`
+              name                     | default
+              ${"AUSTENITE_BOOLEAN_A"} | ${true}
+              ${"AUSTENITE_BOOLEAN_B"} | ${false}
+            `(
+              "returns the default ($name)",
+              ({ name, default: d }: { name: string; default: boolean }) => {
+                const variable = boolean(name, "description-a", {
+                  required: true,
+                  default: d,
+                });
+
+                if (emptyValue != null) process.env[name] = emptyValue;
+                initialize();
+
+                expect(variable.value()).toBe(d);
+              }
+            );
+          });
+        });
+
+        describe("when there is no default value", () => {
+          describe(".value()", () => {
+            it.each`
+              name                     | message
+              ${"AUSTENITE_BOOLEAN_A"} | ${"AUSTENITE_BOOLEAN_A is undefined and does not have a default value"}
+              ${"AUSTENITE_BOOLEAN_B"} | ${"AUSTENITE_BOOLEAN_B is undefined and does not have a default value"}
+            `(
+              "throws ($name)",
+              ({ name, message }: { name: string; message: string }) => {
+                const variable = boolean(name, "description-a", {
+                  required: true,
+                });
+
+                initialize();
+
+                expect(() => {
+                  variable.value();
+                }).toThrow(message);
+              }
+            );
+          });
+        });
+      }
+    );
+  });
+
+  describe("when the variable is optional", () => {
+    it("returns an optional boolean value", () => {
+      const variable = boolean("AUSTENITE_BOOLEAN_A", "description-a", {
+        required: false,
+      });
+
+      initialize();
+      const actual = variable.value();
+
+      expect(hasType<boolean | undefined, typeof actual>(actual)).toBeNull();
+    });
+
+    describe("when the value is one of the accepted literals", () => {
+      describe(".value()", () => {
+        it.each`
+          name                     | value      | expected
+          ${"AUSTENITE_BOOLEAN_A"} | ${"true"}  | ${true}
+          ${"AUSTENITE_BOOLEAN_B"} | ${"false"} | ${false}
+        `(
+          "returns the value ($name)",
+          ({
+            name,
+            value,
+            expected,
+          }: {
+            name: string;
+            value: string;
+            expected: boolean;
+          }) => {
+            const variable = boolean(name, "description-a", {
+              required: false,
+            });
+
+            process.env[name] = value;
+            initialize();
+
+            expect(variable.value()).toBe(expected);
+          }
+        );
+      });
+    });
+
+    describe("when the value is invalid", () => {
+      describe(".value()", () => {
+        it.each`
+          name                     | value      | message
+          ${"AUSTENITE_BOOLEAN_A"} | ${"ture"}  | ${'The value of AUSTENITE_BOOLEAN_A ("ture") is invalid: expected either "true" or "false".'}
+          ${"AUSTENITE_BOOLEAN_B"} | ${"flase"} | ${'The value of AUSTENITE_BOOLEAN_B ("flase") is invalid: expected either "true" or "false".'}
+        `(
+          "throws ($name)",
+          ({
+            name,
+            value,
+            message,
+          }: {
+            name: string;
+            value: string;
+            message: string;
+          }) => {
+            const variable = boolean(name, "description-a", {
+              required: false,
+            });
+
+            process.env[name] = value;
+            initialize();
+
+            expect(() => {
+              variable.value();
+            }).toThrow(message);
+          }
+        );
+      });
+    });
+
+    describe.each`
+      label          | emptyValue
+      ${"undefined"} | ${undefined}
+      ${"empty"}     | ${""}
+    `(
+      "when the value is $label",
+      ({ emptyValue }: { emptyValue: string | undefined }) => {
+        describe("when there is a default value", () => {
+          describe(".value()", () => {
+            it.each`
+              name                     | default
+              ${"AUSTENITE_BOOLEAN_A"} | ${true}
+              ${"AUSTENITE_BOOLEAN_B"} | ${false}
+            `(
+              "returns the default ($name)",
+              ({ name, default: d }: { name: string; default: boolean }) => {
+                const variable = boolean(name, "description-a", {
+                  required: false,
+                  default: d,
+                });
+
+                if (emptyValue != null) process.env[name] = emptyValue;
+                initialize();
+
+                expect(variable.value()).toBe(d);
+              }
+            );
+          });
+        });
+
+        describe("when there is no default value", () => {
+          describe(".value()", () => {
+            it.each`
+              name
+              ${"AUSTENITE_BOOLEAN_A"}
+              ${"AUSTENITE_BOOLEAN_B"}
+            `("returns undefined ($name)", ({ name }: { name: string }) => {
+              const variable = boolean(name, "description-a", {
+                required: false,
+              });
+
+              initialize();
+
+              expect(variable.value()).toBeUndefined();
+            });
+          });
+        });
+      }
+    );
+  });
+});
