22ae67adbf98ed271b7e659a069d8ab1253398ab
Erin Millard <contact@erin.id.au>
2022-10-22 10:58:09 +1000

    Expose integer declarations

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ import { setProcessExit } from "./environment";
 
 setProcessExit(process.exit.bind(process));
 
+export { bigInteger } from "./big-integer";
 export { boolean } from "./boolean";
 export { duration } from "./duration";
 export { enumeration } from "./enumeration";
 export { initialize } from "./environment";
+export { integer } from "./integer";
 export { kubernetesAddress } from "./kubernetes-address";
 export { number } from "./number";
 export { string } from "./string";
