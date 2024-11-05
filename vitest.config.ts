4c828675ff711d5582c9193140492cc9936f45a4
Erin Millard <contact@erin.id.au>
2024-11-05 15:45:02 +1000

    Disable Vitest watch mode

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    watch: false,
     include: ["test/suite/**/*.spec.ts"],
     setupFiles: ["test/setup.ts"],
     typecheck: {
