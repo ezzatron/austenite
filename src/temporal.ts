a6e2d26410fa038d01fedce5c03b460644c64b81
Erin Millard-Wright <contact@erin.id.au>
2026-04-19 12:16:33 +1000

    Ignore Temporal polyfill

--- src/temporal.ts
+++ src/temporal.ts
@@ -1,5 +1,7 @@
+/* v8 ignore start -- @preserve */
 export const Temporal =
   typeof globalThis.Temporal !== "undefined"
     ? globalThis.Temporal
     : ((await import("@js-temporal/polyfill"))
         .Temporal as unknown as typeof globalThis.Temporal);
+/* v8 ignore stop -- @preserve */
